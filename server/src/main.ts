ed5a2b2c808ba08b06a21eca21637492dca277bc
techmefr <compigni.gaetan@gmail.com>
2024-09-09 16:21:13 +0200

    reinit main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,30 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Server } from 'socket.io';
-import { roomHandler } from './room/room';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const server = app.getHttpServer(); 
-  const io = new Server(server, {
-    cors: {
-      origin: "*", 
-      methods: ["GET", "POST"]
-    }
+  app.enableCors({
+    // TODO changer l'url et mettre dans le .env
+    origin: process.env.CLIENT_URL || 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
   });
 
-  io.on('connection', (socket) => {
-    console.log('User connected');
-
-    roomHandler(socket)
-  
-    socket.on('disconnect', () => {
-      console.log('User disconnected');
-    });
-  });
-
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 
 bootstrap();
