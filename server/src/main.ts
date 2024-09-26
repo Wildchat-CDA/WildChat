d5fad3edaa3c3d967d921da237dce7747d4c9df0
techmefr <compigni.gaetan@gmail.com>
2024-09-26 10:01:26 +0200

    fix eslint error with space

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,8 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule);
 
   app.enableCors({
     origin: 'http://localhost:5173',
@@ -11,9 +10,8 @@ async function bootstrap() {
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
-  
+
   await app.listen(3000);
-  
 }
 
 bootstrap();
